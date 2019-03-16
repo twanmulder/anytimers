4b9181c8281396015925b186515dd011481d5c9c
twanmulder <twan.mulder@gmail.com>
2019-03-16 20:16:48 +0100

    Added css
    This still needs updating/ organizing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+import './css/css.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Login from "./pages/Login";
+import Overview from "./pages/Overview";
+
+const Index = () => {
+  return (
+    <BrowserRouter>
+    <div className="app" id="app">
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/overview" component={Overview}/>
+      </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
