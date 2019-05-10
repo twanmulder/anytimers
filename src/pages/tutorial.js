722d363838b2b1c5bc923d5a6af28c9713889c24
twan <twan.mulder@gmail.com>
2019-05-10 18:20:36 +0200

    Updated tut css
    Updated tutorial css and added 404 to pages

--- src/pages/tutorial.js
+++ src/pages/tutorial.js
@@ -15,45 +15,49 @@ class Tutorial extends React.Component {
     return(
         <div className="wrapper">
           <div className="cross">
-            <i className="fas fa-times"></i>
+            <Link to="/">
+              <i className="fas fa-times"></i>
+            </Link>
           </div>
 
-          <Slider {...settings}>
-            <div>
-              <div className="tutorial tut1">
+          <div className="slider-wrapper">
+            <Slider {...settings}>
+              <div>
+                <div className="tutorial tut1">
 
-                <div className="tuttext">
-                  Add your friends to start tracking anytimers!
-                  </div>
+                  <div className="tuttext">
+                    Add your friends to start tracking anytimers!
+                    </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <div className="tutorial tut1">
+              <div>
+                <div className="tutorial tut1">
 
-                <div className="tuttext">
-                  Create (or join) a group to keep track of your friends.
-                  </div>
+                  <div className="tuttext">
+                    Create (or join) a group to keep track of your friends.
+                    </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <div className="tutorial tut1">
+              <div>
+                <div className="tutorial tut1">
 
-                <div className="tuttext">
-                  Add or drink anytimers bij pressing the beer button.
-                  </div>
+                  <div className="tuttext">
+                    Add or drink anytimers bij pressing the beer button.
+                    </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <div className="tutorial tut4">
+              <div>
+                <div className="tutorial tut4">
 
-                <Link to="/">
-                  <div className="tuttext tuttext4">
-                    That's it! Click me to start tracking those anytimers!
-                  </div>
-                </Link>
+                  <Link to="/">
+                    <div className="tuttext tuttext4">
+                      That's it! Click me to start tracking those anytimers!
+                    </div>
+                  </Link>
+                </div>
               </div>
-            </div>
-          </Slider>
+            </Slider>
+          </div>
         </div>
     )
   }
