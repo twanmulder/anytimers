add0f061276cecc06cedc0ce500eae9a3213eb03
twan <twan.mulder@gmail.com>
2019-05-13 09:29:09 +0200

    Updated Login
    Added password visibility function.(Still needs fixing)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,6 +7,22 @@ class Login extends React.PureComponent {
     document.location.reload();
   };
 
+  // THIS NEEDS A FIX. MAYBE USE TOGGLE OR SOME OTHER WEIRD ES6 STUFF
+  togglePassword = () => {
+    let passwordVis = document.getElementById('password');
+    let eye = document.getElementById('eye');
+
+    if (passwordVis.type === "password") {
+      passwordVis.type = "text";
+      eye.classList.add('fa-eye-slash');
+      eye.classList.remove('fa-eye');
+    } else{
+      passwordVis.type = "password";
+      eye.classList.add('fa-eye');
+      eye.classList.remove('fa-eye-slash');
+    }
+  };
+
   render() {
     const hasCookie = document.cookie.indexOf("signed_in") > -1;
     return (
@@ -28,7 +44,12 @@ class Login extends React.PureComponent {
             </div>
             <div className="input-type">
               <label htmlFor="password">Password</label>
-              <input type="password" placeholder="•••••••••" id="password"></input>
+              <div className="password-input">
+                <input type="password" placeholder="•••••••••" id="password"></input>
+                <div onClick={this.togglePassword}>
+                  <i id="eye" className="fas fa-eye"></i>
+                </div>
+              </div>
               <div className="forgot-password">
                 <Link to="forgotpassword">
                   <b>Forgot</b> your password?
