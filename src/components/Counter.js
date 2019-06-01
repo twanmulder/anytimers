3202bd04efe6d02da9ed7d459c51735c2b1bd967
twan <twan.mulder@gmail.com>
2019-06-01 16:35:45 +0200

    Added counter component to update page

---
+++ src/components/Counter.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class Counter extends React.Component{
+    // PASS COUNT AS PROP
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    // COUNT INCREASE TO STATE PROPS
+    increment() {
+        this.setState({
+          count: this.state.count + 1
+        });
+      };
+      
+    // COUNT DECREASE TO STATE PROPS
+    decrement() {
+        if(this.state.count === 0) {
+            this.setState({
+                counter: 0
+            });
+        }else {
+            this.setState({
+                count: this.state.count - 1
+            });
+            };
+        }
+
+    render() {
+        return(
+            <div className="amount">
+            <div className="minus" onClick={(e) => this.decrement(e)}>
+              <i className="fas fa-minus"></i>
+            </div>
+            <div className="number">
+              <p id="drinkValue">{this.state.count}</p>
+            </div>
+            <div className="plus" onClick={(e) => this.increment(e)}>
+              <i className="fas fa-plus"></i>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Counter;
