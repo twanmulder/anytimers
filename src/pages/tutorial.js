9d37808bbd508c5a4a520ddffdbc3ff7a1db004c
twan <twan.mulder@gmail.com>
2019-08-04 15:57:53 +0200

    Added function to click buttons in onBoarding

--- src/pages/tutorial.js
+++ src/pages/tutorial.js
@@ -11,13 +11,31 @@ class Tutorial extends React.Component {
 					h1: "Add Friends",
 					p: "Search and add your friends to start tracking your anytimers."
 				},
-				{ h1: "Add Anytimers", p: "Add anytimers using the 'beer' button." },
-				{ h1: "Drink Anytimers", p: "Drink anytimers and have fun!" }
+				{
+					h1: "Add Anytimers",
+					p: "Add a from, to, amount, and description to your anytimer."
+				},
+				{
+					h1: "Drink Anytimers",
+					p: "Drink anytimers using the same 'beer' button. Have fun!"
+				}
 			],
 			toOverview: false
 		};
 	}
-	handleClick = () => {
+
+	dotClick = index => {
+		// Toggle active dot class
+		document
+			.querySelectorAll(".onBoarding-dot")
+			[this.state.frame].classList.toggle("active");
+		document
+			.querySelectorAll(".onBoarding-dot")
+			[index].classList.toggle("active");
+		this.setState({ frame: index });
+	};
+
+	nextClick = () => {
 		// If the button is clicked on final frame, redirect to overview page
 		if (this.state.frame === 2) {
 			this.setState({ toOverview: true });
@@ -47,13 +65,22 @@ class Tutorial extends React.Component {
 					<p>{this.state.frames[this.state.frame].p}</p>
 				</div>
 				<div className="onBoarding-controls">
-					<button className="onBoarding-button" onClick={this.handleClick}>
+					<button className="onBoarding-button" onClick={this.nextClick}>
 						Next
 					</button>
 					<div className="onBoarding-dots">
-						<div className="onBoarding-dot active"></div>
-						<div className="onBoarding-dot"></div>
-						<div className="onBoarding-dot"></div>
+						<div
+							className="onBoarding-dot active"
+							onClick={() => this.dotClick(0)}
+						></div>
+						<div
+							className="onBoarding-dot"
+							onClick={() => this.dotClick(1)}
+						></div>
+						<div
+							className="onBoarding-dot"
+							onClick={() => this.dotClick(2)}
+						></div>
 					</div>
 				</div>
 			</div>
