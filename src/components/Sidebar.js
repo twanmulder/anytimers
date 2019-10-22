a8251783e01b2330a64c0ad848856885d1bd9f93
TwanMulder <twan.mulder@gmail.com>
2019-10-22 17:33:23 +0200

    Fixed nested link inside li element

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Sidebar extends React.PureComponent {
-	deleteCookie = () => {
-		document.cookie = "signed_in= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-		document.location.reload();
-	};
+  deleteCookie = () => {
+    document.cookie = 'signed_in= ; expires = Thu, 01 Jan 1970 00:00:00 GMT'
+    document.location.reload()
+  }
 
-	render() {
-		return (
-			<div
-				className={
-					this.props.isAsideToggled ? "profileAside toggled" : "profileAside"
-				}
-			>
-				<div className="profileDetail">
-					<div className="profileAvatar">
-						<div className="profileAvatarCircle"></div>
-					</div>
-					<div className="profileName">
-						<p>Twan Mulder</p>
-					</div>
-					<ul>
-						<Link to="/addfriends">
-							<li>Add friends</li>
-						</Link>
-						<li>Profile</li>
-						<li>Settings</li>
-					</ul>
-					<div className="profileAsideLogOut" onClick={this.deleteCookie}>
-						<p>Log out</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div
+        className={
+          this.props.isAsideToggled ? 'profileAside toggled' : 'profileAside'
+        }
+      >
+        <div className="profileDetail">
+          <div className="profileAvatar">
+            <div className="profileAvatarCircle"></div>
+          </div>
+          <div className="profileName">
+            <p>Twan Mulder</p>
+          </div>
+          <ul>
+            <li>
+              <Link to="/addfriends">Add friends</Link>
+            </li>
+            <li>Profile</li>
+            <li>Settings</li>
+          </ul>
+          <div className="profileAsideLogOut" onClick={this.deleteCookie}>
+            <p>Log out</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Sidebar;
+export default Sidebar
