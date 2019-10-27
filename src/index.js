82a947e185d181fdae626fc203677ee47cfe717f
TwanMulder <twan.mulder@gmail.com>
2019-10-27 13:38:01 +0100

    Initiliazed Firebase instance in components/Firebase

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
-import "./css/css.scss";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker'
+import './css/css.scss'
 
-import Overview from "./pages/Overview";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Forgotpassword from "./pages/Forgotpassword";
-import Register from "./pages/Register";
-import Tutorial from "./pages/Tutorial";
-import Fournulfour from "./pages/Fournulfour";
-import Add from "./pages/Add";
-import Drink from "./pages/Drink";
-import AddFriends from "./pages/addFriends";
+import Firebase, { FirebaseContext } from './components/Firebase'
+
+import Overview from './pages/Overview'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Forgotpassword from './pages/Forgotpassword'
+import Register from './pages/Register'
+import Tutorial from './pages/Tutorial'
+import Fournulfour from './pages/Fournulfour'
+import Add from './pages/Add'
+import Drink from './pages/Drink'
+import AddFriends from './pages/addFriends'
 
 const Index = () => {
-	return (
-		<BrowserRouter>
-			<div className="app" id="app">
-				<Switch>
-					<Route path="/landing" component={Landing} />
-					<Route path="/login" component={Login} />
-					<Route path="/forgotpassword" component={Forgotpassword} />
-					<Route path="/register" component={Register} />
-					<Route path="/tutorial" component={Tutorial} />
-					<Route path="/add" component={Add} />
-					<Route path="/drink" component={Drink} />
-					<Route path="/addfriends" component={AddFriends} />
-					<Route exact path="/" component={Overview} />
+  return (
+    <BrowserRouter>
+      <div className="app" id="app">
+        <Switch>
+          <Route path="/landing" component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/forgotpassword" component={Forgotpassword} />
+          <Route path="/register" component={Register} />
+          <Route path="/tutorial" component={Tutorial} />
+          <Route path="/add" component={Add} />
+          <Route path="/drink" component={Drink} />
+          <Route path="/addfriends" component={AddFriends} />
+          <Route exact path="/" component={Overview} />
 
-					<Route path="/" component={Fournulfour} />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
-};
+          <Route path="/" component={Fournulfour} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <Index />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register()
