1219d18da748443cd3da07389ce456cf45a54a0e
TwanMulder <twan.mulder@gmail.com>
2019-10-27 15:29:58 +0100

    Removed Cookie Check

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,45 +1,42 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-import NoAnytimers from "../components/noAnytimers";
-import BeerButtons from "../components/beerButtons";
-import AnytimersGiveList from "../components/anytimersGiveList";
-import AnytimersReceiveList from "../components/anytimersReceiveList";
-import Header from "../components/Header";
+import NoAnytimers from '../components/noAnytimers'
+import BeerButtons from '../components/beerButtons'
+import AnytimersGiveList from '../components/anytimersGiveList'
+import AnytimersReceiveList from '../components/anytimersReceiveList'
+import Header from '../components/Header'
 
-import anytimers from "../content/anytimers";
+import anytimers from '../content/anytimers'
 
 class Overview extends React.PureComponent {
-	isEmpty = anytimers => {
-		if (Object.keys(anytimers).length === 0) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-	render() {
-		const hasCookie = document.cookie.indexOf("signed_in") > -1;
-
-		return (
-			<div className="wrapper">
-				{!hasCookie && <Redirect to="/landing" />}
-				<Header />
-				{/* If the anytimers object is empty, add noAnytimers component */
-				/* Else, add the beerbuttons and the anytimers components  */}
-				{this.isEmpty(anytimers) ? (
-					<NoAnytimers />
-				) : (
-					[
-						<BeerButtons />,
-						<div id="anytimers">
-							<AnytimersGiveList anytimers={anytimers} />
-							<AnytimersReceiveList />
-						</div>
-					]
-				)}
-			</div>
-		);
-	}
+  isEmpty = anytimers => {
+    if (Object.keys(anytimers).length === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        {/* If the anytimers object is empty, add noAnytimers component */
+        /* Else, add the beerbuttons and the anytimers components  */}
+        {this.isEmpty(anytimers) ? (
+          <NoAnytimers />
+        ) : (
+          [
+            <BeerButtons />,
+            <div id="anytimers">
+              <AnytimersGiveList anytimers={anytimers} />
+              <AnytimersReceiveList />
+            </div>,
+          ]
+        )}
+      </div>
+    )
+  }
 }
 
-export default Overview;
+export default Overview
