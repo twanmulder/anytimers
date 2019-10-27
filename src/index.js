954758b5e500ef69122515d43685b952585fa8c6
TwanMulder <twan.mulder@gmail.com>
2019-10-27 14:51:57 +0100

    Redid routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import './css/css.scss'
+import * as ROUTES from '../src/constants/routes'
 
 import Firebase, { FirebaseContext } from './components/Firebase'
 
@@ -22,15 +23,15 @@ const Index = () => {
     <BrowserRouter>
       <div className="app" id="app">
         <Switch>
-          <Route path="/landing" component={Landing} />
-          <Route path="/login" component={Login} />
-          <Route path="/forgotpassword" component={Forgotpassword} />
-          <Route path="/register" component={Register} />
-          <Route path="/tutorial" component={Tutorial} />
-          <Route path="/add" component={Add} />
-          <Route path="/drink" component={Drink} />
-          <Route path="/addfriends" component={AddFriends} />
-          <Route exact path="/" component={Overview} />
+          <Route path={ROUTES.LANDING} component={Landing} />
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.FORGOT_PASSWORD} component={Forgotpassword} />
+          <Route path={ROUTES.REGISTER} component={Register} />
+          <Route path={ROUTES.TUTORIAL} component={Tutorial} />
+          <Route path={ROUTES.ADD} component={Add} />
+          <Route path={ROUTES.DRINK} component={Drink} />
+          <Route path={ROUTES.ADD_FRIENDS} component={AddFriends} />
+          <Route exact path={ROUTES.OVERVIEW} component={Overview} />
 
           <Route path="/" component={Fournulfour} />
         </Switch>
