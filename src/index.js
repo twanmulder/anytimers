11b022225cc8c0edc6273def2a2aa8c2c2f06d88
TwanMulder <twan.mulder@gmail.com>
2019-10-28 13:18:17 +0100

    Rewrote index.js to state component
    We now have auth state management! (sort of)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import './css/css.scss'
 import * as ROUTES from '../src/constants/routes'
+import { compose } from 'recompose'
 
 import Firebase, { FirebaseContext } from './components/Firebase'
+import { withFirebase } from './components/Firebase'
 
 import Overview from './pages/Overview'
 import Landing from './pages/Landing'
@@ -18,27 +20,68 @@ import Add from './pages/Add'
 import Drink from './pages/Drink'
 import AddFriends from './pages/addFriends'
 
-const Index = () => {
-  return (
-    <BrowserRouter>
-      <div className="app" id="app">
-        <Switch>
-          <Route path={ROUTES.LANDING} component={Landing} />
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.FORGOT_PASSWORD} component={Forgotpassword} />
-          <Route path={ROUTES.REGISTER} component={Register} />
-          <Route path={ROUTES.TUTORIAL} component={Tutorial} />
-          <Route path={ROUTES.ADD} component={Add} />
-          <Route path={ROUTES.DRINK} component={Drink} />
-          <Route path={ROUTES.ADD_FRIENDS} component={AddFriends} />
-          <Route exact path={ROUTES.OVERVIEW} component={Overview} />
+// const Index = () => {
+//   return (
+//     <BrowserRouter>
+//       <div className="app" id="app">
+//         <Switch>
+//           <Route path={ROUTES.LANDING} component={Landing} />
+//           <Route path={ROUTES.LOGIN} component={Login} />
+//           <Route path={ROUTES.FORGOT_PASSWORD} component={Forgotpassword} />
+//           <Route path={ROUTES.REGISTER} component={Register} />
+//           <Route path={ROUTES.TUTORIAL} component={Tutorial} />
+//           <Route path={ROUTES.ADD} component={Add} />
+//           <Route path={ROUTES.DRINK} component={Drink} />
+//           <Route path={ROUTES.ADD_FRIENDS} component={AddFriends} />
+//           <Route exact path={ROUTES.OVERVIEW} component={Overview} />
 
-          <Route path="/" component={Fournulfour} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  )
+//           <Route path="/" component={Fournulfour} />
+//         </Switch>
+//       </div>
+//     </BrowserRouter>
+//   )
+// }
+
+class IndexBase extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      authUser: null,
+    }
+  }
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser ? this.setState({ authUser }) : this.setState({ authUser: null })
+    })
+  }
+  componentWillUnmount() {
+    this.listener()
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app" id="app">
+          <Switch>
+            <Route path={ROUTES.LANDING} component={Landing} />
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.FORGOT_PASSWORD} component={Forgotpassword} />
+            <Route path={ROUTES.REGISTER} component={Register} />
+            <Route path={ROUTES.TUTORIAL} component={Tutorial} />
+            <Route path={ROUTES.ADD} component={Add} />
+            <Route path={ROUTES.DRINK} component={Drink} />
+            <Route path={ROUTES.ADD_FRIENDS} component={AddFriends} />
+            <Route exact path={ROUTES.OVERVIEW} component={Overview} />
+
+            <Route path="/" component={Fournulfour} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
+
+const Index = compose(withFirebase)(IndexBase)
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
