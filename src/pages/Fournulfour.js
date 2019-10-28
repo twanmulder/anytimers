e7fee2db6d6f91539c09006e1e8bf0d3af842dcb
TwanMulder <twan.mulder@gmail.com>
2019-10-28 17:18:53 +0100

    404 now links to landing

--- src/pages/Fournulfour.js
+++ src/pages/Fournulfour.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../constants/routes'
 
 class Fournulfour extends React.Component {
-	render() {
-		return (
-			<div id="fournulfour">
-				<div className="veralign">
-					<h1>
-						OOPSIE WOOPSIE YOU MADE A FUCKY WUCKY <br />
-					</h1>
-					<Link to="/">
-						<h1 className="fournulfourbutton">CLICK ME TO GO BACK</h1>
-					</Link>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="fournulfour">
+        <div className="veralign">
+          <h1>
+            OOPSIE WOOPSIE YOU MADE A FUCKY WUCKY <br />
+          </h1>
+          <Link to={ROUTES.LANDING}>
+            <h1 className="fournulfourbutton">CLICK ME TO GO BACK</h1>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Fournulfour;
+export default Fournulfour
