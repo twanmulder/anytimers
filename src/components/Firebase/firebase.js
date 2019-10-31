13e9fa407c8b30d24007d01dfc73b65b35113df1
TwanMulder <twan.mulder@gmail.com>
2019-10-31 23:56:34 +0100

    Added basic firebase store

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/database'
 require('dotenv').config()
 
 const config = {
@@ -15,6 +16,7 @@ class Firebase {
     app.initializeApp(config)
 
     this.auth = app.auth()
+    this.db = app.database()
   }
   // *** Auth API ***
   doCreateUserWithEmailAndPassword = (email, password) =>
