e9d48bc5e1515e797624cc1ece2b1c419f982d0d
TwanMulder <twan.mulder@gmail.com>
2019-11-03 20:08:15 +0100

    Started working on firestore
    Boy do I not like backend dev

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { compose } from 'recompose'
+import { withFirebase } from '../components/Firebase'
 
 import NoAnytimers from '../components/noAnytimers'
 import BeerButtons from '../components/beerButtons'
@@ -8,7 +10,7 @@ import Header from '../components/Header'
 
 import anytimers from '../content/anytimers'
 
-class Overview extends React.PureComponent {
+class OverviewBase extends React.PureComponent {
   isEmpty = anytimers => {
     if (Object.keys(anytimers).length === 0) {
       return true
@@ -16,7 +18,29 @@ class Overview extends React.PureComponent {
       return false
     }
   }
+
+  testGetData = () => {
+    const currenUserUid = this.props.firebase.auth.W
+    const db = this.props.firebase.db
+    console.log(currenUserUid)
+    // let docRef = db.collection('anytimers').where('to', '==', currenUserUid)
+    // docRef
+    //   .get()
+    //   .then(function(doc) {
+    //     if (doc.exists) {
+    //       console.log('Document data:', doc.data())
+    //     } else {
+    //       // doc.data() will be undefined in this case
+    //       console.log('No such document!')
+    //     }
+    //   })
+    //   .catch(function(error) {
+    //     console.log('Error getting document:', error)
+    //   })
+  }
+
   render() {
+    this.testGetData()
     return (
       <div className="wrapper">
         <Header />
@@ -37,5 +61,7 @@ class Overview extends React.PureComponent {
     )
   }
 }
+
+const Overview = compose(withFirebase)(OverviewBase)
 
 export default Overview
