5eb2bbc1828f374951534238122cf907cbf9b875
TwanMulder <twan.mulder@gmail.com>
2019-11-18 17:41:29 +0100

    Maybe fix for mobile address bar shite
    Delete if doesnt work

--- src/index.js
+++ src/index.js
@@ -20,7 +20,17 @@ class IndexBase extends Component {
       loading: true,
     }
   }
+  redefineVHonResize() {
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+
   componentDidMount() {
+    this.redefineVHonResize()
     this.setState({ loading: true })
 
     this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
