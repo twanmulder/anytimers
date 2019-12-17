883b1e6aa4e30624875820d65e2de97614593c07
TwanMulder <twan.mulder@gmail.com>
2019-12-17 23:30:07 +0100

    Added dynamic anytimers list
    content is still static though :')

--- src/components/Anytimers.js
+++ src/components/Anytimers.js
@@ -1,5 +1,39 @@
 import React from 'react'
 
+import anytimers from '../content/anytimers'
+
+class Anytimer extends React.Component {
+  render() {
+    return (
+      <li className="anytimer-item is-revealing">
+        <h4 className="anytimer-names">
+          {this.props.from}
+          <span className="anytimer-amount">{this.props.amount}</span>
+          <span className="anytimer-arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-arrow-right"
+            >
+              <line x1="5" y1="12" x2="19" y2="12" />
+              <polyline points="12 5 19 12 12 19" />
+            </svg>
+          </span>
+          {this.props.to}
+        </h4>
+        <p className="anytimer-description">{this.props.description}</p>
+      </li>
+    )
+  }
+}
+
 class Anytimers extends React.Component {
   render() {
     return (
@@ -8,56 +42,15 @@ class Anytimers extends React.Component {
           <div className="hero-inner">
             <h4 className="anytimer-subtitle">Recent anytimers</h4>
             <ul className="anytimer-list">
-              <li className="anytimer-item is-revealing">
-                <h4 className="anytimer-names">
-                  Boyen
-                  <span className="anytimer-amount">4</span>
-                  <span className="anytimer-arrow">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-arrow-right"
-                    >
-                      <line x1="5" y1="12" x2="19" y2="12" />
-                      <polyline points="12 5 19 12 12 19" />
-                    </svg>
-                  </span>
-                  Wouter
-                </h4>
-                <p className="anytimer-description">Omdat hij een mafklap is</p>
-              </li>
-              <li className="anytimer-item is-revealing">
-                <h4 className="anytimer-names">
-                  Daan
-                  <span className="anytimer-amount">13</span>
-                  <span className="anytimer-arrow">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-arrow-right"
-                    >
-                      <line x1="5" y1="12" x2="19" y2="12" />
-                      <polyline points="12 5 19 12 12 19" />
-                    </svg>
-                  </span>
-                  Joep
-                </h4>
-                <p className="anytimer-description">5 flessen ice trekken</p>
-              </li>
+              {anytimers.map((any, i) => (
+                <Anytimer
+                  to={any.to}
+                  from={any.from}
+                  amount={any.amount}
+                  description={any.description}
+                  key={i}
+                />
+              ))}
             </ul>
           </div>
         </div>
---
+++ src/content/anytimers.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    from: 'Boyen',
+    to: 'Wouter',
+    description: 'Omdat hij een mafklap is',
+    amount: 4,
+  },
+  {
+    from: 'Daan',
+    to: 'Joep',
+    description: '5 flessen ice trekken',
+    amount: 13,
+  },
+]
