94c09b3e73bfb2415976fa3ba5744a55766a2193
TwanMulder <twan.mulder@gmail.com>
2019-12-17 22:57:32 +0100

    Updated SVGs with JSX tags

--- src/components/Anytimers.js
+++ src/components/Anytimers.js
@@ -20,9 +20,9 @@ class Anytimers extends React.Component {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="feather feather-arrow-right"
                     >
                       <line x1="5" y1="12" x2="19" y2="12" />
@@ -45,9 +45,9 @@ class Anytimers extends React.Component {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="feather feather-arrow-right"
                     >
                       <line x1="5" y1="12" x2="19" y2="12" />
