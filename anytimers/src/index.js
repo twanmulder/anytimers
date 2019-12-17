c80fe218ca864a18bc59ba5353dabf3f93fe4b13
TwanMulder <twan.mulder@gmail.com>
2019-12-17 21:47:59 +0100

    Updated react base render to root component
    Apprently the body isn't "cool" enough

--- anytimers/src/index.js
+++ anytimers/src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.querySelector('body')
+  document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
