b5111dc10b5dbd01e0034e6a05aef16b5815fe56
TwanMulder <twan.mulder@gmail.com>
2019-12-17 22:54:30 +0100

    Anytimers is now only visible on mobile

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,20 @@ import Hero from './Hero'
 import Anytimers from './Anytimers'
 
 class Main extends React.Component {
+  state = {
+    isMobile: false,
+  }
+
+  componentDidMount() {
+    this.setState({ isMobile: window.innerWidth <= 1024 })
+  }
+
   render() {
+    const { isMobile } = this.state
     return (
       <main style={{ flexDirection: 'column' }}>
         <Hero></Hero>
-        <Anytimers></Anytimers>
+        {isMobile ? '' : <Anytimers></Anytimers>}
       </main>
     )
   }
