4ff9c9c8d5f79d06508e2dab258f20758bb8c163
TwanMulder <twan.mulder@gmail.com>
2020-01-30 22:26:51 +0100

    Fixed small JSX mistake in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
     return (
       <footer>
         <ul>
-          <li class="footer-copyright">
+          <li className="footer-copyright">
             <span>Anytimers</span>
             <span>© Copyright 2020 Anytimers Software.</span>
             <span> All rights reserved.</span>
