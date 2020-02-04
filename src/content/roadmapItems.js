219d9ff47810f16f7729eb8041022ee20c0236bc
TwanMulder <twan.mulder@gmail.com>
2020-02-05 00:17:57 +0100

    Created dynamic roadmap
    Still needs work on mobile styling

---
+++ src/content/roadmapItems.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: '2012',
+    description:
+      'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quisiuvaret expetendis his, te elit voluptua dignissim per, habeo iustoprimis ea eam.',
+  },
+  {
+    title: '2013',
+    description:
+      'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quisiuvaret expetendis his, te elit voluptua dignissim per, habeo iustoprimis ea eam.',
+  },
+  {
+    title: '2014',
+    description:
+      'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quisiuvaret expetendis his, te elit voluptua dignissim per, habeo iustoprimis ea eam.',
+  },
+  {
+    title: '2015',
+    description:
+      'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quisiuvaret expetendis his, te elit voluptua dignissim per, habeo iustoprimis ea eam.',
+  },
+  {
+    title: '2016',
+    description:
+      'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quisiuvaret expetendis his, te elit voluptua dignissim per, habeo iustoprimis ea eam.',
+  },
+  {
+    title: '2017',
+    description:
+      'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quisiuvaret expetendis his, te elit voluptua dignissim per, habeo iustoprimis ea eam.',
+  },
+]
--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -1,6 +1,31 @@
 import React, { Fragment } from 'react'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Form from '../components/Form'
+
+import roadmapItems from '../content/roadmapItems'
+
+class RoadmapItem extends React.Component {
+  render() {
+    const { title, description } = this.props
+
+    return (
+      <div className="container">
+        <div className="content">
+          <h2>{title}</h2>
+          <p>
+            {description}
+            Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+            admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis
+            iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto
+            primis ea eam.
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
 
 class Roadmap extends React.Component {
   render() {
@@ -9,10 +34,17 @@ class Roadmap extends React.Component {
         <Header />
 
         <main>
-          <section className="section">
-            <h1>Anytimers Roadmap</h1>
+          <section className="section section-roadmap">
+            <h3>Anytimers Roadmap</h3>
+
+            <div className="timeline">
+              {roadmapItems.map(item => <RoadmapItem title={item.title} description={item.description} key={item.title} />)}
+            </div>
           </section>
         </main>
+
+        <Form />
+        <Footer />
       </Fragment>
     )
   }
