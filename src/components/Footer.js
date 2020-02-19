ca91ab6e17b7c00d3ca140047248e4624e2daef6
TwanMulder <twan.mulder@gmail.com>
2020-02-19 21:05:01 +0100

    Started working on updated footer
    Building socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,35 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <ul>
+        <ul className="footer-social-wrapper">
+          <li className="footer-social">
+            <a
+              href="https://twitter.com/AnytimersApp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li className="footer-social">
+            <a
+              href="mailto:info@anytimers.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Email
+            </a>
+          </li>
+        </ul>
+
+        <ul className="footer-copyright-wrapper">
           <li className="footer-copyright">
             <span>Anytimers</span>
+          </li>
+          <li className="footer-copyright">
             <span>© Copyright 2020 Anytimers Software.</span>
+          </li>
+          <li className="footer-copyright">
             <span> All rights reserved.</span>
           </li>
         </ul>
