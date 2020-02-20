9434064784e898bad7e996dcbe67b962a647e6a9
TwanMulder <twan.mulder@gmail.com>
2020-02-20 09:27:46 +0100

    Working on footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,38 +4,40 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <ul className="footer-social-wrapper">
-          <li className="footer-social">
-            <a
-              href="https://twitter.com/AnytimersApp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-          </li>
-          <li className="footer-social">
-            <a
-              href="mailto:info@anytimers.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Email
-            </a>
-          </li>
-        </ul>
+        <section>
+          <ul className="footer-social-wrapper">
+            <li className="footer-social">
+              <a
+                href="https://twitter.com/AnytimersApp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </li>
+            <li className="footer-social">
+              <a
+                href="mailto:info@anytimers.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Email
+              </a>
+            </li>
+          </ul>
 
-        <ul className="footer-copyright-wrapper">
-          <li className="footer-copyright">
-            <span>Anytimers</span>
-          </li>
-          <li className="footer-copyright">
-            <span>© Copyright 2020 Anytimers Software.</span>
-          </li>
-          <li className="footer-copyright">
-            <span> All rights reserved.</span>
-          </li>
-        </ul>
+          <ul className="footer-copyright-wrapper">
+            <li className="footer-copyright">
+              <span>Anytimers</span>
+            </li>
+            <li className="footer-copyright">
+              <span>© Copyright 2020 Anytimers Software.</span>
+            </li>
+            <li className="footer-copyright">
+              <span> All rights reserved.</span>
+            </li>
+          </ul>
+        </section>
       </footer>
     )
   }
