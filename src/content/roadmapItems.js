f299e17fd07961ad6a46308846ea1545255b472a
TwanMulder <twan.mulder@gmail.com>
2020-02-21 14:01:25 +0100

    Roadmap items are updated to public release

--- src/content/roadmapItems.js
+++ src/content/roadmapItems.js
@@ -1,27 +1,27 @@
 export default [
   {
-    title: 'Private Beta',
+    title: 'Alpha Release',
     description:
-      'Anytimers will first become available as a private beta release. A select amount of users will be allowed to download/use the app.',
+      'Anytimers will first become available in an alpha state. Basic functionality and design will be included.',
   },
   {
     title: 'User Feedback',
     description:
-      'We hope to receive feedback from these users regarding problems they encounter, and possibly provide advice for new features.',
+      'We hope to receive feedback from our users regarding problems they encounter, and provide advice for new features.',
   },
   {
-    title: 'Anytimers V2',
+    title: 'Anytimers V1',
     description:
       "Using this feedback, we'll create a revamped version of the Anytimers app. This app will contain useful new features, a modernized style, and a greatly improved user experience.",
   },
   {
-    title: 'Public Release!',
+    title: 'Release!',
     description:
-      'After polishing V2, we intend upon releasing the Anytimers app publicly. This way, a mostly polished app will be launched.',
+      'After polishing V1, we intend upon releasing it. Now, a greatly more usable and enjoyable experience is available to our users (you!).',
   },
   {
     title: 'And then...',
     description:
-      "As soon as the app is publicly launched, our intention is to keep working on improving its experience. Next to that, we'll also keep creating new features, based on your feedback.",
+      "As the updated app is released, our intention is to keep working on improving its experience. Next to that, we'll also keep creating new features, based on your feedback.",
   },
 ]
